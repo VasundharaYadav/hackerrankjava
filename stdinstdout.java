1355da1eb6ac4d24bf6a5e9d0a9e63031e316a51
Vasundhara Yadav <vasundharayadav222@gmail.com>
2025-01-09 19:02:53 +0530

    Add files via upload

---
+++ stdinstdout.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+       System.out.println("enter 3 interger");
+        int a = scan.nextInt();
+
+        System.out.println(a);
+        
+    }
+}
